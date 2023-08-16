80e379a4ccfe113571270cb0dc2ac702c7fdca70
Kourtney Boston <86276317+kboston91@users.noreply.github.com>
2023-08-15 22:51:23 -0500

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="container">
+    <h1 className='header'>To do List</h1>
+    <p>This app serves as a resource of tasks you need to accomplish for the day.</p>
+    
+    </div>
+    
+
+    </div>
+  );
+}
+
+export default App;
